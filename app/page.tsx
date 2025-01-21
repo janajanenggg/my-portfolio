6ee3a398e44306b8376fdc0e1ded923fc89e42da
Janajane Teneza <teneza.janajane202045@gmail.com>
2025-01-21 17:22:41 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
-export default function Home() {
+const Home = () => {
   return (
-  <div>
-    Janajane
-  </div>
+    <>
+      <Navbar />
+      <Hero />
+      <div className="max-w-6xl mx-auto px-4">
+        {/* Add other sections here */}
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
