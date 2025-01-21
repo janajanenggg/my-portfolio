d8253ab4fb4a76c5b936aed859688d0b22f4158d
Janajane Teneza <teneza.janajane202045@gmail.com>
2025-01-21 18:54:37 +0800

    changes

---
+++ app/test-page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+      <div className="bg-green-500 text-white min-h-screen flex items-center justify-center">
+        <h1 className="text-5xl font-bold">Tailwind is working!</h1>
+      </div>
+    );
+  }
+  
